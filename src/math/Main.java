a74c8784b425e2e1c734b9fd9323129882bac704
FireBreathing-Lizard <bivan535@gmail.com>
2021-11-15 16:52:48 +0500

    2.0

--- src/math/Main.java
+++ src/math/Main.java
@@ -1,12 +1,15 @@
 package math;
 
+import com.sun.xml.internal.bind.v2.TODO;
+
 import java.util.Scanner;
 
 public class Main {
 
-    public static float a, b, c, D, X1;
-    public static double x1, x2, X;
-    public static String symb;
+    public static float a, b, c, D, mc, f1, f2;
+    public static double x1, x2, X, kor, Minx, Maxx;
+    public static String symb, convert;
+
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -23,66 +26,119 @@ public class Main {
         c = scan.nextFloat();
         scan.nextLine();
 
+        System.out.print("Symbol is: ");
+        symb = scan.nextLine();
+
+        D = (b * b - 4 * a * c);
+        kor = Math.sqrt(D);
+
         printLineOrNot();
     }
 
-    public static void printLineOrNot() {
-        if (D >= 0) {
-            if (a != 0) {
-                System.out.println("Квадратная функция");
-                printFunc();
-                printDisc();
-                printx1x2();
-            } else {
-                System.out.println("Линейная функция");
-                printLineFunc();
-            }
+
+    private static void printLineOrNot() {
+        if (a == 0) {
+            System.out.println("Линейное неравенство:\n ");
+
+            System.out.printf("%.0fx + %.0f %s 0%n", b, c, symb);
+            System.out.printf("%.0fx %s %.0f%n", b, symb, mc = c * -1);
+            System.out.printf("x %s %.0f%n", change(b, symb), mc / b);
+
         } else {
-            System.out.println("No answers");
+            System.out.println("Квадратное неравенство:\n ");
+
+            convert = String.format("%.0fx² + %.0fx + %.0f %s 0%n", a, b, c, symb);
+            convert = convert.contains("+ -") ? convert.replace("+ -", "- ") : convert;
+            System.out.println(convert);
+            neravenstvo();
+
+            printx1x2();
         }
+    }
+
+
+    private static void neravenstvo() {
+        if (a < 0) {
+            a = a * -1;
+            b = b * -1;
+            c = c * -1;
+            symb = symb.contains(">") ? symb.replace(">", "<") : symb.replace("<", ">");
+            convert = String.format("%.0fx² + %.0fx + %.0f %s 0%n", a, b, c, symb);
+            convert = convert.contains("+ -") ? convert.replace("+ -", "- ") : convert;
+            System.out.println(convert);
+        }
+    }
+
+    public static String change(float num, String symbol) {
+        if (num > 0) {
+            symbol = symbol.replace(">", "<");
+        } else {
+            symbol = symbol.replace("<", ">");
+        }
+        return symbol;
     }
 
     public static void printx1x2() {
-        if (D == 0) {
-            X = (-b) / (2 * a);
-            System.out.printf("X = %.1f", X);
-        } else {
-            prx1();
-            System.out.println("");
-            prx2();
+        if (D < 0) {
+            System.out.println("Нет корней");
+            return;
         }
+
+        //DiscAndKor();
+        if (D == 0) {
+
+            X = (-b) / (2 * a);
+            x1 = (X - 1);
+            x2 = (X + 1);
+            f1 = (float) (a * x1 * x1 + b * x1 + c);
+            f2 = (float) (a * x2 * x2 + b * x2 + c);
+
+            if ((symb.equals(">") && f1 < 0 && f2 < 0) || (symb.equals("<") && f1 > 0 && f2 > 0)) {
+                System.out.println("Неравенство не имеет решений");
+            }
+            if ((symb.equals(">=") && f1 < 0 && f2 < 0) || (symb.equals("<=") && f1 > 0 && f2 > 0)) {
+                System.out.printf("X = %.0f%n", X);
+            }
+
+            if ((symb.contains(">") && f1 > 0) || (symb.contains("<") && f1 < 0)) {
+                System.out.printf("X %s %.1f%n", symb.contains("=") ? ">=" : ">", X);
+            }
+            if ((symb.contains(">") && f2 > 0) || (symb.contains("<") && f2 < 0)) {
+                System.out.printf("X %s %.1f%n", symb.contains("=") ? "<=" : "<", X);
+            }
+
+        } else {
+            prx12();
+        }
+
     }
 
-    public static void printFunc() {
-        System.out.printf("y = %.1fx^2 + %.1fx + %.1f%n", a, b, c);
-    }
-
-    public static void printDisc() {
-        D = (b * b - 4 * a * c);
-        //System.out.printf("D = %.1f * %.1f - 4 * %.1f * %.1f = %.1f%n", b, b, a, c, D);
-    }
-
-    public static void prx1() {
+    public static void prx12() {
         x1 = (-b + Math.sqrt(D)) / (2 * a);
-        System.out.printf("X1 = %.1f", x1);
-    }
-
-    public static void prx2() {
         x2 = (-b - Math.sqrt(D)) / (2 * a);
-        System.out.printf("X2 = %.1f", x2);
+
+        Minx = Math.min(x1, x2);
+        Maxx = Math.max(x1, x2);
+
+        if (symb.contains(">")) {
+            System.out.printf("X %s %.0f, X %s %.0f%n", change(a, symb), Minx, symb, Maxx);
+        } else {
+            System.out.printf("%.0f %s X %s %.0f%n", Minx, symb, symb, Maxx);
+        }
+
+        //System.out.printf("X1 = %.2f%n", Minx);
+        //System.out.printf("X2 = %.2f%n", Maxx);
     }
 
-    public static void printLineFunc(){
-        Scanner scan = new Scanner(System.in);
-
-        System.out.printf("y = %.1fx + %.1f%n", b, c);
-        System.out.printf("%.1fx + %.1f = 0%n", b, c);
-
-        System.out.print("Symbol is: ");
-        symb = scan.nextLine();
-        System.out.printf("%.1fx + %.1f %s 0%n", b, c, symb);
-
-        X1 = c/b;
-        System.out.printf("x = %.1f", X1);
+    public static void DiscAndKor() {
+        System.out.println(D);
+        System.out.println(kor);
     }
+
+
 }
+/*
+symbol = symbol.contains(">") ? symbol.replace(">", "<") : symbol.replace("<", ">");
+        System.out.printf("%.0f %s X %s %.0f %n", Minx, neravenstvo(a, symb), neravenstvo(a, symb), Maxx);
+        return symbol;
+ */
